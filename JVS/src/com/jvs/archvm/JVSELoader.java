882458be29dc20aefa63cd2920bc5e86b6a7ec79
PH4N70M <yugrvaniya@gmail.com>
2025-05-29 17:56:27 +0530

    Completed the ArchVM Virtual Machine.

--- JVS/src/com/jvs/archvm/JVSELoader.java
+++ JVS/src/com/jvs/archvm/JVSELoader.java
@@ -21,7 +21,6 @@ public class JVSELoader
      */
     public static JVSEInstructions load(String filePath)
     {
-        System.out.println("\n\n");
         Map<Integer, Instruction> instructions = new HashMap<>();
         Map<String, Integer> methods = new HashMap<>();
         int lineCount = 1;
