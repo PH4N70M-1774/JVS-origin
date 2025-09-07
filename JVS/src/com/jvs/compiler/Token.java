663bab1b50c5557a1d56daf8e7d078cd9862da2c
PH4N70M <yugrvaniya@gmail.com>
2025-09-07 18:42:36 +0530

    Added getters for type and lexeme.

--- JVS/src/com/jvs/compiler/Token.java
+++ JVS/src/com/jvs/compiler/Token.java
@@ -13,6 +13,14 @@ public class Token {
         this.lexeme = lexeme;
     }
 
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
     @Override
     public String toString(){
         return type+((lexeme==null)?"":"("+lexeme+")");
